010d1668a4d5d1129f9c55da516aae36c7dccf8e
two-dont-sleep <holleratyaboyout@gmail.com>
2022-02-11 13:56:52 +0300

    Fully adapted for mobile and tablet devises

--- src/js/script.js
+++ src/js/script.js
@@ -6,6 +6,7 @@ window.addEventListener('DOMContentLoaded', () => {
     hamburger.addEventListener('click', () => {
         hamburger.classList.toggle('hamburger_active');
         menu.classList.toggle('menu_active');
+
     });
 
     menuItem.forEach(item => {
